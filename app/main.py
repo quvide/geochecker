d6a71fe1e43e92aeb381695cf5072d73d4eaca73
Elias Benkhodja <elias.benkhodja@gmail.com>
2017-02-10 13:57:55 +0200

    refactor and move rate handling to client side

--- app/main.py
+++ app/main.py
@@ -1,11 +1,7 @@
-from flask import Flask, jsonify, render_template, request, abort
+from flask import Flask, jsonify, render_template, request, abort, Blueprint, g
 from redis import Redis
 import requests
 import ruamel.yaml as yaml
-
-app = Flask(__name__)
-redis = Redis()
-
 import time
 from collections import deque
 
@@ -36,6 +32,16 @@ class Limiter:
 
 limits = {}
 
+def get_ratelimit():
+    ratelimit_key = str(request.remote_addr)
+    if not ratelimit_key in limits:
+        limits[ratelimit_key] = Limiter(g.coursedata["limits"]["req"], g.coursedata["limits"]["time"])
+    return limits[ratelimit_key]
+
+
+app = Flask(__name__)
+redis = Redis()
+course = Blueprint("course", __name__, url_prefix="/<course>")
 
 with open("config.yaml", "r") as f:
     CONFIG = yaml.safe_load(f)
@@ -44,50 +50,63 @@ with open("config.yaml", "r") as f:
 if not CONFIG or not DATA:
     exit("Missing configuration file or it is invalid!")
 
-@app.route("/<course>", methods=["GET", "POST"])
-def index(course):
-    if not course in DATA:
+@course.url_value_preprocessor
+def pull_course(endpoint, values):
+    course = values.pop("course")
+    if course in DATA:
+        g.coursedata = DATA[course]
+        g.course = course
+    else:
         abort(404)
 
-    course = DATA[course]
+@course.route("/")
+def index():
+    return render_template("course.j2", title=g.coursedata["title"],
+                tasks=g.coursedata["tasks"], captcha_key=CONFIG["captcha"]["key"])
 
-    ratelimit_key = str(request.remote_addr)
-    if not ratelimit_key in limits:
-        limits[ratelimit_key] = Limiter(course["limits"]["req"], course["limits"]["time"])
-    ratelimit = limits[ratelimit_key]
 
-    if request.method == "GET":
-        return render_template("course.j2", title=course["title"],
-                tasks=course["tasks"], captcha_key=CONFIG["captcha"]["key"], limits=limits[ratelimit_key])
+@course.route("/check", methods=["POST"])
+def check():
+    data = request.form.to_dict()
+    ratelimit = get_ratelimit()
+    res = {}
 
-    else:
-        data = request.form.to_dict()
-        res = {}
+    # captcha
+    url = "https://www.google.com/recaptcha/api/siteverify"
+    response = data["g-recaptcha-response"]
 
-        # captcha
-        url = "https://www.google.com/recaptcha/api/siteverify"
-        response = data["g-recaptcha-response"]
+    r = requests.post(url, data={"secret": CONFIG["captcha"]["secret"],
+             "response": response}).json()
+    if r["success"] and ratelimit.request_available():
+        res["fields"] = {}
 
-        r = requests.post(url, data={"secret": CONFIG["captcha"]["secret"],
-                 "response": response}).json()
-        if r["success"] and ratelimit.request_available():
-            res["fields"] = {}
-
-            wrong = False
-            for key, val in course["tasks"].items():
-                if key in data and data[key] and int(data[key]) == val["val"]:
-                    res["fields"][key] = "ok"
-                else:
-                    res["fields"][key] = "wrong"
-                    wrong = True
-
-            if not wrong:
-                res["coordinates"] = course["coordinates"]
+        wrong = False
+        for key, val in g.coursedata["tasks"].items():
+            if key in data and data[key] and int(data[key]) == val["val"]:
+                res["fields"][key] = "ok"
             else:
-                # don't count correct answers to rate limit
-                ratelimit.add()
+                res["fields"][key] = "wrong"
+                wrong = True
 
-        res["captcha_success"] = r["success"]
-        res["available_requests"] = ratelimit.available_requests()
+        if not wrong:
+            res["coordinates"] = g.coursedata["coordinates"]
+        else:
+            # don't count correct answers to rate limit
+            ratelimit.add()
 
-        return jsonify(res)
+    res["captcha_success"] = r["success"]
+
+    return jsonify(res)
+
+@course.route("/rate", methods=["POST"])
+def rate():
+    ratelimit = get_ratelimit()
+    res = {
+        "available_requests": ratelimit.available_requests(),
+        "max_requests": ratelimit.max_requests,
+        "time": ratelimit.time
+    }
+
+    return jsonify(res)
+
+app.register_blueprint(course)
--- app/static/main.js
+++ app/static/main.js
@@ -1,9 +1,27 @@
 $(document).ready(function() {
+	function update_available_requests() {
+		$.post("rate", function(data, status, res) {
+			$("#available-requests").text(data.available_requests);
+			if (data.available_requests === 0) {
+				$("#submit-button").attr("disabled", "true")
+			} else {
+				$("#submit-button").removeAttr("disabled");
+			}
+
+			$("#max-requests").text(data.max_requests);
+			$("#time").text(data.time/60);
+		});
+	}
+
+	update_available_requests();
+	setInterval(update_available_requests, "10000");
+
 	$("#form").submit(function(event) {
 	  event.preventDefault();
 
-		$.post("#", $(this).serializeArray(), function(data, status, res) {
+		$.post("check", $(this).serializeArray(), function(data, status, res) {
 			grecaptcha.reset();
+			update_available_requests();
 			if (data.captcha_success) {
 				$("#status").html("");
 				for (var attr in data.fields) {
@@ -16,11 +34,6 @@ $(document).ready(function() {
 				}
 			} else {
 				$("#status").html("<span class=\"error\">Muista täyttää CAPTCHA!</span>");
-			}
-
-			$("#available-requests").text(data.available_requests);
-			if (data.available_requests === 0) {
-				$("#submit-button").prop("disabled", "true")
 			}
 		});
 	});
